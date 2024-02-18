75e2d76aefec41c44f98e89a0899e6d5338b125e
fami-fish <fami-fish@proton.me>
2024-02-18 13:28:43 +0200

    Find midpoint of spline with ctrl click

--- client/editor.js
+++ client/editor.js
@@ -1,14 +1,14 @@
-import {AbstractStage} from '/client/lib/abstract';
-import {AnalysisStage} from '/client/lib/analysis';
-import {BridgesStage} from '/client/lib/bridges';
-import {OrderStage} from '/client/lib/order';
-import {PathStage} from '/client/lib/path';
-import {StrokesStage} from '/client/lib/strokes';
-import {VerifiedStage} from '/client/lib/verified';
-import {assert} from '/lib/base';
-import {cjklib} from '/lib/cjklib';
+import { AbstractStage } from "/client/lib/abstract";
+import { AnalysisStage } from "/client/lib/analysis";
+import { BridgesStage } from "/client/lib/bridges";
+import { OrderStage } from "/client/lib/order";
+import { PathStage } from "/client/lib/path";
+import { StrokesStage } from "/client/lib/strokes";
+import { VerifiedStage } from "/client/lib/verified";
+import { assert } from "/lib/base";
+import { cjklib } from "/lib/cjklib";
 
-Session.set('editor.glyph', undefined);
+Session.set("editor.glyph", undefined);
 
 const stages = {
   analysis: AnalysisStage,
@@ -18,27 +18,28 @@ const stages = {
   strokes: StrokesStage,
   verified: VerifiedStage,
 };
-const types = ['path', 'bridges', 'strokes', 'analysis', 'order', 'verified'];
+const types = ["path", "bridges", "strokes", "analysis", "order", "verified"];
 let last_glyph = undefined;
 let last_json = undefined;
-let stage = new AbstractStage;
+let stage = new AbstractStage();
 
 const changeGlyph = (method, argument) => {
-  argument = argument || Session.get('editor.glyph');
-  Meteor.call(method, argument, function(error, data) {
+  argument = argument || Session.get("editor.glyph");
+  Meteor.call(method, argument, function (error, data) {
     assert(!error, error);
-    Session.set('editor.glyph', data);
+    Session.set("editor.glyph", data);
     window.location.hash = encodeURIComponent(data.character);
   });
-}
+};
 
 const clearStageSessionKeys = (type) => {
-  Object.keys(Session.keys).filter((x) => x.startsWith(`stages.${type}.`))
-                           .map((x) => Session.set(x, undefined));
-}
+  Object.keys(Session.keys)
+    .filter((x) => x.startsWith(`stages.${type}.`))
+    .map((x) => Session.set(x, undefined));
+};
 
 const constructStage = (type) => {
-  const glyph = Session.get('editor.glyph');
+  const glyph = Session.get("editor.glyph");
   const current = glyph.stages[type];
   if (!current || current.sentinel) {
     glyph.stages[type] = null;
@@ -48,15 +49,15 @@ const constructStage = (type) => {
   assert(stage.type === type);
   stage.forceRefresh = forceRefresh;
   stage.forceRefresh(true /* from_construct_stage */);
-}
+};
 
 const forceRefresh = (from_construct_stage) => {
-  const glyph = Session.get('editor.glyph');
+  const glyph = Session.get("editor.glyph");
   stage.refreshUI(glyph.character, glyph.metadata);
   let output = stage.getStageOutput();
   const current = glyph.stages[stage.type];
   if (from_construct_stage && (!current || current.sentinel)) {
-    output = {sentinel: true};
+    output = { sentinel: true };
   }
   if (!_.isEqual(output, current)) {
     glyph.stages[stage.type] = output;
@@ -65,9 +66,9 @@ const forceRefresh = (from_construct_stage) => {
         glyph.stages[types[i]] = null;
       }
     }
-    Session.set('editor.glyph', glyph);
+    Session.set("editor.glyph", glyph);
   }
-}
+};
 
 const incrementStage = (amount) => {
   const index = types.indexOf(stage.type);
@@ -82,80 +83,89 @@ const incrementStage = (amount) => {
       // dependency of the current glyph is incomplete. Switch to it. This code
       // is a terrible hack because it makes use of the string used to render
       // the incomplete-component message.
-      const log = Session.get('stage.status');
-      const prefix = 'Incomplete components: ';
+      const log = Session.get("stage.status");
+      const prefix = "Incomplete components: ";
       if (log.length > 0 && log[0].message.startsWith(prefix)) {
-        changeGlyph('getGlyph', log[0].message[prefix.length]);
+        changeGlyph("getGlyph", log[0].message[prefix.length]);
       }
       return;
     }
     stage.forceRefresh();
   }
   constructStage(types[new_index]);
-}
+};
 
 const loadCharacter = () => {
   const character = decodeURIComponent(window.location.hash.slice(1));
-  const glyph = Session.get('editor.glyph');
+  const glyph = Session.get("editor.glyph");
   if (!character) {
-    changeGlyph('getNextGlyph');
+    changeGlyph("getNextGlyph");
   } else if (!glyph || glyph.character !== character) {
-    changeGlyph('getGlyph', character);
+    changeGlyph("getGlyph", character);
   }
-}
+};
 
 const resetStage = () => {
-  const glyph = Session.get('editor.glyph');
+  const glyph = Session.get("editor.glyph");
   glyph.stages[stage.type] = null;
-  Session.set('editor.glyph', glyph);
+  Session.set("editor.glyph", glyph);
   constructStage(stage.type);
-}
+};
 
 const bindings = {
-  a: () => changeGlyph('getPreviousGlyph'),
-  A: () => changeGlyph('getPreviousUnverifiedGlyph'),
-  q: () => changeGlyph('getPreviousVerifiedGlyph'),
-  d: () => changeGlyph('getNextGlyph'),
-  D: () => changeGlyph('getNextUnverifiedGlyph'),
-  e: () => changeGlyph('getNextVerifiedGlyph'),
+  a: () => changeGlyph("getPreviousGlyph"),
+  A: () => changeGlyph("getPreviousUnverifiedGlyph"),
+  q: () => changeGlyph("getPreviousVerifiedGlyph"),
+  d: () => changeGlyph("getNextGlyph"),
+  D: () => changeGlyph("getNextUnverifiedGlyph"),
+  e: () => changeGlyph("getNextVerifiedGlyph"),
   r: resetStage,
   s: () => incrementStage(1),
   w: () => incrementStage(-1),
+  // ctrl + click adds a point onto the nearset spline
 };
 
 // We avoid arrow functions in this map so that this is bound to the template.
 Template.editor.events({
-  'click svg .selectable': function(event) {
+  "click svg .selectable": function (event) {
     stage.handleEvent(event, this);
     stage.forceRefresh();
-  }
+  },
+  "click svg": function (e) {
+    if (stage.type === types[1] && e.ctrlKey) {
+      stage.createPoint(e);
+    }
+  },
 });
 
 Template.editor.helpers({
-  paths: () => Session.get('stage.paths'),
-  lines: () => Session.get('stage.lines'),
-  points: () => Session.get('stage.points'),
-  animations: () => Session.get('stage.animations'),
+  paths: () => Session.get("stage.paths"),
+  lines: () => Session.get("stage.lines"),
+  points: () => Session.get("stage.points"),
+  animations: () => Session.get("stage.animations"),
 });
 
 Template.status.helpers({
-  stage: () => Session.get('stage.type'),
-  template: () => `${Session.get('stage.type')}_stage`,
-  lines: () => Session.get('stage.status'),
+  stage: () => Session.get("stage.type"),
+  template: () => `${Session.get("stage.type")}_stage`,
+  lines: () => Session.get("stage.status"),
 });
 
 Tracker.autorun(() => {
-  const glyph = Session.get('editor.glyph');
+  const glyph = Session.get("editor.glyph");
   const json = JSON.stringify(glyph);
   if (!glyph || json === last_json) {
     return;
   } else if (!last_glyph || glyph.character !== last_glyph.character) {
     let last_completed_stage = types[0];
-    types.map((x) => { if (glyph.stages[x]) last_completed_stage = x; });
+    types.map((x) => {
+      if (glyph.stages[x]) last_completed_stage = x;
+    });
     constructStage(last_completed_stage);
   } else {
-    Meteor.call('saveGlyph', glyph,
-                (error, data) => { if (error) console.error(error) });
+    Meteor.call("saveGlyph", glyph, (error, data) => {
+      if (error) console.error(error);
+    });
     if (!_.isEqual(glyph.metadata, last_glyph.metadata)) {
       stage.refreshUI(glyph.character, glyph.metadata);
     }
@@ -165,16 +175,23 @@ Tracker.autorun(() => {
 });
 
 Meteor.startup(() => {
-  $('body').on('keypress', (event) => {
+  $("body").on("keypress", (event) => {
     const key = String.fromCharCode(event.which);
     if (bindings.hasOwnProperty(key)) {
       bindings[key]();
-    } else if ('1' <= key && key <= '9') {
-      const index = key.charCodeAt(0) - '1'.charCodeAt(0);
-      const href = $('.metadata .reference')[index].href;
-      window.open(href, '_blank').focus();
+    } else if ("1" <= key && key <= "9") {
+      const index = key.charCodeAt(0) - "1".charCodeAt(0);
+      const href = $(".metadata .reference")[index].href;
+      window.open(href, "_blank").focus();
     }
   });
-  $(window).on('hashchange', loadCharacter);
+  $("body").on("mousemove", (e) => {
+    var svg = $("svg g");
+    var p = new DOMPoint(e.pageX, e.pageY);
+    var svgp = p.matrixTransform(svg[0].getScreenCTM().inverse());
+
+    $(".coord").text(`(${Math.round(svgp.x)},${Math.round(svgp.y)})`);
+  });
+  $(window).on("hashchange", loadCharacter);
   cjklib.promise.then(loadCharacter).catch(console.error.bind(console));
 });
--- client/lib/bridges.js
+++ client/lib/bridges.js
@@ -1,19 +1,19 @@
-import {AbstractStage} from '/client/lib/abstract';
-import {Point} from '/lib/base';
-import {stroke_extractor} from '/lib/stroke_extractor';
+import { AbstractStage } from "/client/lib/abstract";
+import { assert, Point } from "/lib/base";
+import { stroke_extractor, Endpoint } from "/lib/stroke_extractor";
 
-const bridgeKey = (bridge) => bridge.map(Point.key).join('-');
+const bridgeKey = (bridge) => bridge.map(Point.key).join("-");
 
 const removeBridge = (bridges, bridge) => {
   const keys = {};
   keys[bridgeKey(bridge)] = true;
   keys[bridgeKey([bridge[1], bridge[0]])] = true;
   return bridges.filter((bridge) => !keys[bridgeKey(bridge)]);
-}
+};
 
 class BridgesStage extends AbstractStage {
   constructor(glyph) {
-    super('bridges');
+    super("bridges");
     const bridges = stroke_extractor.getBridges(glyph.stages.path);
     this.original = bridges.bridges;
     this.adjusted = glyph.stages.bridges || this.original;
@@ -40,49 +40,151 @@ class BridgesStage extends AbstractStage {
     }
     this.adjusted.push(bridge);
   }
+
   handleEvent(event, template) {
     if (template.x1 !== undefined) {
-      const bridge = [[template.x1, template.y1], [template.x2, template.y2]];
+      const bridge = [
+        [template.x1, template.y1],
+        [template.x2, template.y2],
+      ];
       this.handleClickOnBridge(bridge);
     } else if (template.cx !== undefined) {
       this.handleClickOnPoint([template.cx, template.cy]);
     }
   }
+
+  createPoint(event) {
+    const svg = $("svg g");
+    var p = new DOMPoint(event.pageX, event.pageY);
+
+    var sp = p.matrixTransform(svg[0].getScreenCTM().inverse());
+    sp = [sp.x, sp.y];
+    console.log(sp);
+
+    var min = Infinity;
+    var closest = this.endpoints[0];
+
+    this.endpoints
+      .map((e) => {
+        if (e.segments[0].control === undefined) {
+          return [Point.midpoint(e.segments[0].start, e.segments[0].end), e];
+        } else {
+          return [e.segments[0].control, e];
+        }
+      })
+      .forEach(([a, e]) => {
+        var dist = Point.distance2(a, sp);
+        if (dist < min) {
+          min = dist;
+          closest = e;
+        }
+      });
+
+    var cp = closest.point;
+    var seg = closest.segments;
+    assert(seg.length === 2, "Not enough segments!");
+    console.log(seg[0]);
+    var start = seg[0].start;
+    var end = seg[0].end;
+
+    var t = 0.5; //TODO: binary search for closest xy
+    var x;
+    var y;
+    if (seg[0].control === undefined) {
+      [x, y] = Point.add(Point.scale(end, t), Point.scale(start, 1 - t));
+    } else {
+      x =
+        (1 - t) * (1 - t) * seg[0].start[0] +
+        2 * (1 - t) * t * seg[0].control[0] +
+        t * t * seg[0].end[0];
+      y =
+        (1 - t) * (1 - t) * seg[0].start[1] +
+        2 * (1 - t) * t * seg[0].control[1] +
+        t * t * seg[0].end[1];
+    }
+
+    //`M ${cp.x} ${cp.y}`
+    const entry = {
+      cls: "success",
+      message:
+        `closest: (${cp[0]},${cp[1]})\n` +
+        `start:   (${start[0]},${start[1]})\n` +
+        `end:     (${end[0]},${end[1]})`,
+    };
+    Session.set("stage.points", [
+      ...[start, end].map((i) => {
+        return {
+          cls: "selectable",
+          cx: i[0],
+          cy: i[1],
+          fill: "green",
+          stroke: "black",
+        };
+      }),
+      {
+        cls: "selectable",
+        cx: x,
+        cy: y,
+        fill: "red",
+        stroke: "black",
+      },
+    ]);
+    // Session.set("stage.paths", [
+    //   { d: this.path, fill: "gray", stroke: "black" },
+    //   {
+    //     d: `M ${seg[0].start[0]} ${seg[0].start[1]} Q ${seg[0].control[0]} ${seg[0].control[1]} ${seg[0].end[0]} ${seg[0].end[1]}`,
+    //     fill: "none",
+    //     stroke: "orange",
+    //   },
+    // ]);
+    Session.set("stage.status", [entry]);
+  }
+
   refreshUI() {
-    Session.set('stage.paths', [{d: this.path, fill: 'gray', stroke: 'gray'}]);
+    Session.set("stage.paths", [
+      { d: this.path, fill: "gray", stroke: "gray" },
+    ]);
     const keys = {};
     this.original.map((bridge) => {
       keys[bridgeKey(bridge)] = true;
       keys[bridgeKey([bridge[1], bridge[0]])] = true;
     });
-    Session.set('stage.lines', this.adjusted.map((bridge) => ({
-      cls: 'selectable',
-      stroke: keys[bridgeKey(bridge)] ? 'red' : 'purple',
-      x1: bridge[0][0],
-      y1: bridge[0][1],
-      x2: bridge[1][0],
-      y2: bridge[1][1],
-    })));
-    Session.set('stage.points', this.endpoints.map((endpoint) => {
-      let color = endpoint.corner ? 'red' : 'black';
-      if (this.selected_point &&
-          Point.equal(endpoint.point, this.selected_point)) {
-        color = 'purple';
-      }
-      return {
-        cls: 'selectable',
-        cx: endpoint.point[0],
-        cy: endpoint.point[1],
-        fill: color,
-        stroke: color,
-      }
-    }));
+    Session.set(
+      "stage.lines",
+      this.adjusted.map((bridge) => ({
+        cls: "selectable",
+        stroke: keys[bridgeKey(bridge)] ? "red" : "purple",
+        x1: bridge[0][0],
+        y1: bridge[0][1],
+        x2: bridge[1][0],
+        y2: bridge[1][1],
+      })),
+    );
+    Session.set(
+      "stage.points",
+      this.endpoints.map((endpoint) => {
+        let color = endpoint.corner ? "red" : "black";
+        if (
+          this.selected_point &&
+          Point.equal(endpoint.point, this.selected_point)
+        ) {
+          color = "purple";
+        }
+        return {
+          cls: "selectable",
+          cx: endpoint.point[0],
+          cy: endpoint.point[1],
+          fill: color,
+          stroke: color,
+        };
+      }),
+    );
     const strokes = stroke_extractor.getStrokes(this.path, this.adjusted);
     const n = strokes.strokes.length;
-    const message = `Extracted ${n} stroke${n == 1 ? '' : 's'}.`;
-    const entry = {cls: 'success', message: message};
-    Session.set('stage.status', strokes.log.concat([entry]));
+    const message = `Extracted ${n} stroke${n == 1 ? "" : "s"}.`;
+    const entry = { cls: "success", message: message };
+    Session.set("stage.status", strokes.log.concat([entry]));
   }
 }
 
-export {BridgesStage};
+export { BridgesStage };
